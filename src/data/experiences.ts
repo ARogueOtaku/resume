76baa3824499434d989157e524d9f53d2b1759e7
ARogueOtaku <amit.rockinad.das@gmail.com>
2024-06-16 15:03:31 +0530

    Added experience for latest company

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -2,12 +2,41 @@ import { WorkExperience } from "@/data/types";
 
 const experiences: WorkExperience[] = [
   {
+    position: "Senior Software Engineer",
+    employer: "LearningMate Solutions",
+    location: "Kolkata",
+    remote: false,
+    contractor: false,
+    start: new Date(2023, 6),
+    clients: ["Imagine Learning"],
+    technologies: [
+      "React",
+      "Redux",
+      "JavaScript",
+      "TypeScript",
+      "GraphQL",
+      "Ant Design",
+      "React Router",
+      "Java",
+      "Kotlin",
+    ],
+    description: [
+      "Developed key features and modules as a Senior Software Engineer at LearningMate for the Cosmos project.",
+      "Cosmos is a React-based single-page application (SPA) designed to author content for Imagine Learning, a US-based ed-tech company focused on digitizing education.",
+      "Built reusable components & collaborated with peers to determine the structure and design of the web app, ensuring scalable and maintainable code.",
+      "Worked on the backend API service called Aether, a microservice that integrates business logic with generic APIs and serves as a bridge between Cosmos and the Imagine Learning backend.",
+      "Conducted code reviews and wrote unit tests to maintain high code quality.",
+      "Worked closely with the client Product and UX teams to gather requirements and provide updates, ensuring the application meets their needs.",
+    ],
+  },
+  {
     position: "FrontEnd Developer",
     employer: "Paytm",
     location: "Bengaluru",
     remote: true,
     contractor: true,
     start: new Date(2023, 1),
+    end: new Date(2023, 6),
     clients: ["Nagad"],
     technologies: [
       "React",
@@ -29,7 +58,7 @@ const experiences: WorkExperience[] = [
     employer: "Thoughtworks",
     location: "Bengaluru",
     remote: true,
-    contractor: false,
+    contractor: true,
     start: new Date(2022, 0),
     end: new Date(2022, 11),
     clients: ["Standard Chartered Bank"],
@@ -55,6 +84,7 @@ const experiences: WorkExperience[] = [
     remote: true,
     contractor: false,
     start: new Date(2021, 10),
+    end: new Date(2023, 6),
     clients: ["Thoughtworks", "Paytm"],
     description: [
       "Built multiple UI modules as Contractor for Thoughtworks and Paytm",
