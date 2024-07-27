7ce22d2888287be32bf3f41b659afd90309ae9dd
Amit Das <amit.rockinad.das@gmail.com>
2024-07-27 18:05:43 +0530

    Update Download.tsx
    Update download link with latest Resume PDF

--- src/components/logical/Download.tsx
+++ src/components/logical/Download.tsx
@@ -4,7 +4,7 @@ function Download() {
       id="download"
       title="Download"
       target="_blank"
-      href="https://drive.google.com/uc?export=download&amp;id=1--d6BnPOaFY7VbD0hWhhgHvZRuypT4lj"
+      href="https://drive.usercontent.google.com/uc?id=1NYHfS0pY4ndob5NxdUIG_22rDXaUOKyx&export=download"
       className="fixed right-0 top-0 my-3 grid cursor-pointer place-items-center bg-slate-800 px-3 py-1 text-2xl text-slate-100 hover:scale-x-110 md:my-0 md:px-4 md:py-2 md:text-4xl"
     >
       ↓
