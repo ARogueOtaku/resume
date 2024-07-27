8516f311f1b2135fa98012fcdfbb928d8b5bc08f
Amit Das <amit.rockinad.das@gmail.com>
2024-07-27 17:57:19 +0530

    Update basicInfo.ts
    Updated basic info

--- src/data/basicInfo.ts
+++ src/data/basicInfo.ts
@@ -4,7 +4,7 @@ const basicInfo: BasicInfo = {
   name: "Amit Das",
   job: "FrontEnd Developer",
   aboutMe:
-    "I am a mid-level React developer with 5+ years of experience in building web applications. My passion lies in creating exceptional user experiences through front-end design and layout, while my ability to handle complex features with ease ensures high performance and minimal bugs. I have expertise in React, Redux, GraphQL, React Router, Material UI, and TailwindCSS, which allows me to bring any idea to life. I have also dabbled in back-end development, writing SQL queries and creating basic REST APIs using NodeJS and Express. I love collaborating with others and thrive in a team environment, and I am always eager to explore new technologies and push the boundaries of what's possible in web development.",
+    "I am a senior-level React developer with 7+ years of experience in building web applications. My passion lies in creating exceptional user experiences through front-end design and layout, while my ability to handle complex features with ease ensures high performance and minimal bugs. I have expertise in React, Redux, GraphQL, React Router, Material UI, and TailwindCSS, which allows me to bring any idea to life. I have also dabbled in back-end development, writing SQL queries and creating basic REST APIs using NodeJS and Express. I love collaborating with others and thrive in a team environment, and I am always eager to explore new technologies and push the boundaries of what's possible in web development.",
 };
 
 export default basicInfo;
